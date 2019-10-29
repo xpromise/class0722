5bb00a955e5a0a85cde0fe360f62e7e41035d091
xiongjian <webjsforyou@163.com>
2019-10-29 11:06:46 +0800

    http客户端和服务端

--- day03/03.http客户端.js
+++ day03/03.http客户端.js
@@ -1,14 +1,30 @@
 /*
   http.request(url[, options][, callback])
+  http.request(options[, callback])
  */
-
 const http = require('http');
 
 // 设置请求信息
-const req = http.request({
+/*const req = http.request({
   hostname: 'localhost', // 主机域名
   port: 3000, // 端口号
   path: '/?username=111&password=222', // 请求路径
+  method: 'GET', // 请求方式
+  headers: { // 请求头信息
+
+  }
+}, function (res) {
+  // 接受响应的回调函数
+  // res相当于可读流
+  res.on('data', (chunk) => {
+    console.log(`响应主体: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('响应中已无数据');
+  });
+});*/
+// 设置请求信息
+const req = http.request('http://localhost:3000?username=111&password=222', {
   method: 'GET', // 请求方式
   headers: { // 请求头信息
 
