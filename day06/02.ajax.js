cb45e88af6b97a2248e274d218c8ad749d0f15bb
xiongjian <webjsforyou@163.com>
2019-11-02 11:19:16 +0800

    jquery的ajax的使用

--- day06/02.ajax.js
+++ day06/02.ajax.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+// 只能解析post请求的urlencoded编码的参数
+app.use(express.urlencoded({extended: true}));
+// 只能解析post请求的json编码的参数
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('服务器返回的响应~');
+  console.log(req.query); // { username: 'jack', age: '18' }
+  res.status(500).send('服务器返回的响应3333~');
+});
+
+app.post('/', (req, res) => {
+  // console.log(req.query);
+  console.log(req.body); // { username: 'jack', age: '18' }
+  res.send('服务器返回的响应3333~');
 });
 
 app.listen(3000, (err) => {
