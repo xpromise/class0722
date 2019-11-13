47f0e17685784b7af0f56ba365b22e3f9faf9031
xiongjian <webjsforyou@163.com>
2019-11-13 11:16:11 +0800

    自定义脚手架

---
+++ 05.react/02.react_demo/01.hello_react/src/index.js
@@ -0,0 +1,10 @@
+/*
+  引入App组件，渲染
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+// 引入App组件
+import App from './App';
+
+// 渲染
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ 05.react/02.react_demo/02.自定义脚手架/src/App.js
@@ -0,0 +1,25 @@
+/*
+  主组件、根组件
+*/
+/* import React from 'react';
+import {Component} from 'react'; */
+// import React, { Component } from 'react';
+import React from 'react';
+
+// 引入图片
+import logo from './assets/images/1.jpg';
+// 引入样式
+import './assets/css/App.css';
+
+// 定义组件
+function App() {
+  return (
+    <div className="box">
+      <h1 className="title">hello react</h1>
+      <img className="logo" src={logo} alt="logo" />
+    </div>
+  );
+}
+
+// 暴露
+export default App;
