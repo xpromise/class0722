70bd5adb097a07f9651515a7b66abe212f07e9a7
xiongjian <webjsforyou@163.com>
2019-11-13 15:42:44 +0800

    添加webpack更新日志

--- 05.react/02.react_demo/03.react_comment/src/components/list/index.jsx
+++ 05.react/02.react_demo/03.react_comment/src/components/list/index.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Item from '../item';
+import Item from "../item";
 
 export default class List extends Component {
   static propTypes = {
-    comments: PropTypes.array.isRequired,
-  }
+    comments: PropTypes.array.isRequired
+  };
 
   render() {
     // 接受props
@@ -15,11 +15,11 @@ export default class List extends Component {
     return (
       <div className="col-md-8">
         <h3 className="reply">评论回复：</h3>
-        <h2 style={{ display: 'none' }}>暂无评论，点击左侧添加评论！！！</h2>
+        <h2 style={{ display: "none" }}>暂无评论，点击左侧添加评论！！！</h2>
         <ul className="list-group">
-          {
-            comments.map((comment, index) => <Item comment={comment} key={index} />)
-          }
+          {comments.map((comment, index) => (
+            <Item comment={comment} key={index} />
+          ))}
         </ul>
       </div>
     );
