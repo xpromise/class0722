e1dfe100bf03814b4c22c7422bec0851201c4c21
xiongjian <webjsforyou@163.com>
2019-11-15 09:19:08 +0800

    ajax请求练习

---
+++ 05.react/01.react_test/11_ajax/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/search/repostories', (req, res) => {
+  const {
+    q,
+    sort
+  } = req.query;
+
+  if (!q || !sort) {
+    res.status(401).send('缺少请求参数');
+  }
+
+  res.json({
+    items: [{
+      html_url: 'https://github.com/axios/axios',
+      name: 'axios'
+    }]
+  });
+})
+
+app.listen(80, (err) => {
+  if (!err) console.log('服务器启动成功');
+  else console.log(err);
+});
